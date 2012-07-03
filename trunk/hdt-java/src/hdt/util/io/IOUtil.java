e8d89b037afbba7bca372d33ee80ab0d50a24cd1
mario.arias@gmail.com <mario.arias@gmail.com@79d02f10-9824-5a6d-8155-d2f4b3853b0f>
2012-07-03 10:49:22 +0000

    Fixed problem reading buffer. Added CountStream
    git-svn-id: https://hdt-java.googlecode.com/svn@13 79d02f10-9824-5a6d-8155-d2f4b3853b0f

--- trunk/hdt-java/src/hdt/util/io/IOUtil.java
+++ trunk/hdt-java/src/hdt/util/io/IOUtil.java
@@ -24,14 +24,14 @@
  *   Miguel A. Martinez-Prieto: migumar2@infor.uva.es
  *   Alejandro Andres:          fuzzy.alej@gmail.com
  */
-
 package hdt.util.io;
 
 import hdt.listener.ProgressListener;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.io.DataInput;
+import java.io.DataOutput;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
@@ -39,7 +39,7 @@ import java.io.OutputStream;
  *
  */
 public class IOUtil {
-	public static String readLine(DataInputStream in, char character) throws IOException {
+	public static String readLine(DataInput in, char character) throws IOException {
 		StringBuilder out = new StringBuilder();
 		while(true) {
 			char c = (char) in.readByte();
@@ -51,7 +51,7 @@ public class IOUtil {
 		return out.toString();
 	}
 	
-	public static String readChars(DataInputStream in, int numChars) throws IOException {
+	public static String readChars(DataInput in, int numChars) throws IOException {
 		StringBuilder out = new StringBuilder();
 		for(int i=0;i<numChars;i++) {
 			char c = (char) in.readByte();
@@ -60,7 +60,7 @@ public class IOUtil {
 		return out.toString();
 	}
 	
-	public static void writeLine(DataOutputStream out, String line) throws IOException {
+	public static void writeLine(DataOutput out, String line) throws IOException {
 		for(int i=0;i<line.length();i++) {
 			byte charByte = (byte) line.charAt(i);
 			out.writeByte(charByte);
@@ -71,6 +71,42 @@ public class IOUtil {
 		// FIXME: Do by blocks and notify listener
 		output.write(buffer, offset, length);
 	}
+	
+	public static void writeLong(OutputStream output, long value) throws IOException {
+		final byte [] writeBuffer = new byte[8];
+        writeBuffer[0] = (byte)(value >>> 56);
+        writeBuffer[1] = (byte)(value >>> 48);
+        writeBuffer[2] = (byte)(value >>> 40);
+        writeBuffer[3] = (byte)(value >>> 32);
+        writeBuffer[4] = (byte)(value >>> 24);
+        writeBuffer[5] = (byte)(value >>> 16);
+        writeBuffer[6] = (byte)(value >>>  8);
+        writeBuffer[7] = (byte)(value >>>  0);
+        output.write(writeBuffer, 0, 8);
+	}
+	
+	public static final long readLong(InputStream input) throws IOException {
+		final byte [] readBuffer = new byte[8];
+		int nRead;
+		int pos=0;
+		while ((nRead = input.read(readBuffer, pos, 8-pos)) >0) {
+			// TODO: Notify progress listener
+			pos += nRead;
+		}
+		
+		if(pos!=8) {
+			throw new IOException("EOF while reading array from InputStream");
+		}
+		
+		return (((long)readBuffer[0] << 56) +
+				((long)(readBuffer[1] & 255) << 48) +
+				((long)(readBuffer[2] & 255) << 40) +
+				((long)(readBuffer[3] & 255) << 32) +
+				((long)(readBuffer[4] & 255) << 24) +
+				((readBuffer[5] & 255) << 16) +
+				((readBuffer[6] & 255) <<  8) +
+				((readBuffer[7] & 255) <<  0));
+	}
 
 	/**
 	 * @param din
@@ -78,11 +114,30 @@ public class IOUtil {
 	 * @param listener
 	 * @return
 	 */
-	public static final byte[] readBuffer(DataInputStream input, int length, ProgressListener listener) throws IOException {
-		// FIXME: Do by blocks and notify listener
-		byte [] buffer = new byte[length];
-		input.read(buffer, 0, length);
-		return buffer;
+	public static final byte[] readBuffer(InputStream input, int length, ProgressListener listener) throws IOException {
+		int nRead;
+		int pos=0;
+		byte[] data = new byte[length];
+
+		while ((nRead = input.read(data, pos, length-pos)) >0) {
+			// TODO: Notify progress listener
+			pos += nRead;
+		}
+		
+		if(pos!=length) {
+			throw new IOException("EOF while reading array from InputStream");
+		}
+
+		return data;
+	}
+	
+	public static CharSequence toBinaryString(long val) {
+		StringBuilder str = new StringBuilder(64);
+		int bits = 64;
+		while(bits-- != 0) {
+			str.append(((val>>>bits) & 1) !=0 ? '1' : '0');
+		}
+		return str;
 	}
 	
 	public static final void printBits(long val, int bits) {
