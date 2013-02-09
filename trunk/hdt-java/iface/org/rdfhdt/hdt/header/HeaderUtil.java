ceb0b1b5809067c106dfb1d8f45bf51e62d9c920
mario.arias <mario.arias@79d02f10-9824-5a6d-8155-d2f4b3853b0f>
2013-02-09 11:37:21 +0000

    Added some utils to simplify adding and extracting data to the header.
    git-svn-id: https://hdt-java.googlecode.com/svn@174 79d02f10-9824-5a6d-8155-d2f4b3853b0f

---
+++ trunk/hdt-java/iface/org/rdfhdt/hdt/header/HeaderUtil.java
@@ -0,0 +1,68 @@
+package org.rdfhdt.hdt.header;
+
+import org.rdfhdt.hdt.exceptions.NotFoundException;
+import org.rdfhdt.hdt.hdt.HDTVocabulary;
+import org.rdfhdt.hdt.triples.IteratorTripleString;
+import org.rdfhdt.hdt.triples.TripleString;
+
+public class HeaderUtil {
+	private static String cleanURI(String uri) {
+		if(uri!=null && uri.length()>=2 && uri.charAt(0)=='<' && uri.charAt(uri.length()-1)=='>') {
+			return uri.substring(1, uri.length()-1);
+		}
+		return uri;
+	}
+	
+	private static String cleanLiteral(String uri) {
+		if(uri!=null && uri.length()>=2 && uri.charAt(0)=='"' && uri.charAt(uri.length()-1)=='"') {
+			return uri.substring(1, uri.length()-1);
+		}
+		return uri;
+	}
+	
+	public static String getProperty(Header header, String subject, String predicate) throws NotFoundException {
+		IteratorTripleString it = header.search(cleanURI(subject), cleanURI(predicate), "");
+        if(it.hasNext()) {
+                TripleString ts = it.next();
+                return ts.getObject().toString();
+        }
+        throw new NotFoundException();
+	}
+	
+	public static int getPropertyInt(Header header, String subject, String predicate) throws NotFoundException {
+		String str = HeaderUtil.getProperty(header, subject, predicate);
+		if(str!=null) {
+			try {
+				return Integer.parseInt(str);
+			} catch(NumberFormatException e) {
+				
+			}
+		}
+		throw new NotFoundException();
+	}
+	
+	public static long getPropertyLong(Header header, String subject, String predicate) throws NotFoundException {
+		String str = HeaderUtil.getProperty(header, subject, predicate);
+		if(str!=null) {
+			try {
+				return Long.parseLong(cleanLiteral(str));
+			} catch(NumberFormatException e) {
+				
+			}
+		}
+		throw new NotFoundException();
+	}
+	
+	public static String getSubject(Header header, String predicate, String object) throws NotFoundException {
+		IteratorTripleString it = header.search("", predicate, object);
+        if(it.hasNext()) {
+                TripleString ts = it.next();
+                return ts.getObject().toString();
+        }
+        throw new NotFoundException();
+	}
+	
+	public static String getBaseURI(Header header) throws NotFoundException {
+        return HeaderUtil.getSubject(header, HDTVocabulary.RDF_TYPE, HDTVocabulary.HDT_DATASET);
+}
+}
