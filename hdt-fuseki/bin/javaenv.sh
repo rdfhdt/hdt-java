eab2782a2b08a47624b6ef9f4e1186d36ec5e0fd
Mario Arias <mario.arias@deri.org>
2013-11-15 12:25:53 +0000

    Fixed assembly

--- hdt-fuseki/bin/javaenv.sh
+++ hdt-fuseki/bin/javaenv.sh
@@ -7,10 +7,10 @@ BASE=`dirname $0`/../
 
 case `uname` in
   CYGWIN*)
-    CP=$( echo "$BASE/target" "$BASE/target/classes" "$BASE"/target/dependency/*.jar . | sed 's/ /;/g')
+    CP=$( echo "$BASE"/lib/*jar "$BASE/target" "$BASE/target/classes" "$BASE"/target/dependency/*.jar . | sed 's/ /;/g')
     ;;
   *)
-    CP=$( echo "$BASE/target" "$BASE/target/classes" "$BASE"/target/dependency/*.jar . | sed 's/ /:/g')
+    CP=$( echo "$BASE"/lib/*jar "$BASE/target" "$BASE/target/classes" "$BASE"/target/dependency/*.jar . | sed 's/ /:/g')
 esac
 #echo $CP
 
