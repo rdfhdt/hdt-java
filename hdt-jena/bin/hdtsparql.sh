1363c16749f7c469edfbd35d203fd0756f1be5bc
Osma Suominen <osma.suominen@helsinki.fi>
2017-02-11 14:35:59 +0200

    fix the hdtsparql wrapper script under hdt-jena to support -stream option

--- hdt-jena/bin/hdtsparql.sh
+++ hdt-jena/bin/hdtsparql.sh
@@ -2,8 +2,13 @@
 
 source `dirname $0`/javaenv.sh
 
+if [[ $1 =~ ^- ]]; then
+	export option=$1
+	shift
+fi
+
 export hdtFile=$1
 shift
-mvn exec:java -Dexec.mainClass="org.rdfhdt.hdtjena.cmd.HDTSparql" -Dexec.args="$hdtFile '$1'"
+mvn exec:java -Dexec.mainClass="org.rdfhdt.hdtjena.cmd.HDTSparql" -Dexec.args="$option $hdtFile '$1'"
 
 exit $?
