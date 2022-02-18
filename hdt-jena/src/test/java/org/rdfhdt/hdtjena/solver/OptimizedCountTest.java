1af4e72e269cc63a67ec261c8b8b0a29d9b0fc52
ate47 <antoine.willerval@the-qa-company.com>
2022-02-18 11:46:39 +0100

    fix hdt-jena override

--- hdt-jena/src/test/java/org/rdfhdt/hdtjena/solver/OptimizedCountTest.java
+++ hdt-jena/src/test/java/org/rdfhdt/hdtjena/solver/OptimizedCountTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertNull;
 import org.junit.Before;
 import org.junit.Test;
 import org.rdfhdt.hdt.enums.ResultEstimationType;
+import org.rdfhdt.hdt.exceptions.NotImplementedException;
 import org.rdfhdt.hdt.hdt.HDT;
 import org.rdfhdt.hdt.hdt.HDTManager;
 import org.rdfhdt.hdt.options.HDTSpecification;
@@ -120,8 +121,8 @@ public class OptimizedCountTest {
 		}
 
 		@Override
-		public long getNextTriplePosition() {
-			return 1;
+		public long getLastTriplePosition() {
+			throw new NotImplementedException();
 		}
 
 	}
